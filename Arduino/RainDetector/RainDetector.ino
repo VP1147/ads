34f442ae76de98ded29540517e004875f1e8a0d4
Vinícius Pavão <viniciuspavao2004@gmail.com>
2022-11-25 18:57:07 -0400

    Update RainDetector.ino

--- Arduino/RainDetector/RainDetector.ino
+++ Arduino/RainDetector/RainDetector.ino
@@ -1,75 +1,77 @@
+#include "DHT.h"
+ 
+#define DHTPIN A2 // pino que estamos conectado
+#define DHTTYPE DHT11 // DHT 11
+
 // Declare I/O pins
 int Sensor = A0;
-int motor = 6;
+int nmotor = 5;
+int pmotor = 6;
 
 // Declare global variables
-int bg;
 long int meter;
 bool IsClosed;
 
+DHT dht(DHTPIN, DHTTYPE);
+
 void setup() {
   Serial.begin(9600);
-  pinMode(motor, OUTPUT);
+  dht.begin();
+  pinMode(pmotor, OUTPUT); pinMode(nmotor, OUTPUT);
   pinMode(Sensor, INPUT);
-  bg = calibrate(Sensor);
-  digitalWrite(motor, LOW);
+  //bg = calibrate(Sensor);
+  digitalWrite(pmotor, LOW); digitalWrite(nmotor, LOW);
 }
 
 void loop() {
-  int Read = abs(analogRead(Sensor)-bg);
-  delay(25);
+  float humid = dht.readHumidity();
+  float temp = dht.readTemperature();
+  int Read = abs(analogRead(Sensor));
+  delay(20);
   Serial.print("\tREAD: "); Serial.print(Read); Serial.print("\t\t");
   Serial.print("MTR: "); Serial.print(meter); Serial.print("\t\t");
   Serial.print("CLSD: "); Serial.print(IsClosed); Serial.print("\t\t");
+  if (! isnan(temp)) { Serial.print("TEMP: "); Serial.print(temp); Serial.print(" C\t\t"); } 
+  if (! isnan(humid)) { Serial.print("HUMID: "); Serial.print(humid); Serial.print(" %\t\t"); }
   Serial.print("\n");
-  if((abs(analogRead(Sensor)-bg)-Read > 15) && meter < 8192) {
-    Serial.println(">> READ");
-    meter += 60;
+  if((abs(analogRead(Sensor)-Read) > 15) && meter < 600) {
+    meter += 25;
   }
   if(meter > 0) { 
-    if(meter > 200 && IsClosed == true) { open(motor); IsClosed = false; }
+    if(meter > 150 && IsClosed == true) { open(); IsClosed = false; }
     meter--;
   }
-  if(meter == 0 && IsClosed == false) { close(motor); IsClosed = true; }
+  if(meter == 0 && IsClosed == false) { close(); IsClosed = true; }
 }
 
-int calibrate(int pin) {
-  // Starts the sensor calibration cycle
-  // Called on setup, runs once
-  Serial.println(" >>> Calibrating <<< ");
-  long int sum = 0;
-  for(int i=0; i<100; i++){
-    sum += analogRead(pin);
-    delay(50);
-  }
-  int bg = sum/100;
-  Serial.println(" >>> Calibrated <<<");
-  Serial.println(" >>> Background value set to "+String(bg)+" <<< ");
-  delay(2000);
-  return bg;
-}
-
-void open(int pin) {
+void open() {
   // Starts the opening cycle
   Serial.println(">> Opening ...");
-  delay(10);
+  //delay(10);
 
   // Work in progress
-  // For now this function only turns on a LED connected to motor port
   //for(int i = 0; i < 256; i++) { analogWrite(pin, i); delay(2); }
-  digitalWrite(pin, HIGH);
-  delay(10);
+
+  int timer = 3000; // milliseconds
+  digitalWrite(pmotor, LOW);
+  digitalWrite(nmotor, HIGH);
+  delay(timer);
+  
+  digitalWrite(pmotor, LOW); digitalWrite(nmotor, LOW);
 }
 
-void close(int pin) {
+void close() {
   // Starts the closing cycle
   Serial.println(">> Closing ...");
-  delay(10);
+  //delay(10);
   
   // Work in progress
-  // For now this function only turns off a LED connected to the motor port
   //for(int i = 255; i > 0; i--) { analogWrite(pin, i); delay(2); }
-  digitalWrite(pin, LOW);
-  delay(10);
 
+  int timer = 3000; // milliseconds
+  digitalWrite(nmotor, LOW);
+  digitalWrite(pmotor, HIGH);
+  delay(timer);
+  
+  digitalWrite(pmotor, LOW); digitalWrite(nmotor, LOW);
 }
