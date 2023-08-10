0e408e9ffd5f8bbe8762aec61842a1cf19a167b0
Vinícius Pavão <viniciuspavao2004@gmail.com>
2023-08-10 08:12:01 -0400

    Update RainDetector.ino

--- Arduino/RainDetector/RainDetector.ino
+++ Arduino/RainDetector/RainDetector.ino
@@ -13,24 +13,19 @@
   UFMS (Federal University of Mato Grosso do Sul)
 */
 
-// Including Adafruit DHT library
-#include "DHT.h" 
-
-// Defining DHT pin and type
-#define DHTPIN A2
-#define DHTTYPE DHT11
-
-
 // Enviromment variables
-#define DHTISUP         false              // Read from DHT temperature-humidity sensor.
-#define NONSTOP         false           // Motor Always on (only for debug-adjustemment).
-                                          // May cause overheating!
-#define SERIALRETURN    false             // Send variable data to serial (debug-adjustemment).
+#define NONSTOP         false                // Motor Always on (only for debug-adjustemment).
+                                             // May cause overheating!
+#define SERIALRETURN    true                // Send verbose variable data to serial (debug or adjustemment).
 
 // Defining I/O pins
-int Sensor = A0;
-int nmotor = 5;
-int pmotor = 6;
+int Sensor = A0;                             // Infrared Sensor
+int nmotor = 5;                              // Close signal pin 
+int pmotor = 6;                              // Open signal pin
+
+// Microswitch pins - Detects if the lid is fully open or fully closed before stopping the motor
+int ms_fullopen = 8;
+int ms_fullclose = 9;
 
 // Declaring global variables
 const long interval = 10000;
@@ -38,20 +33,12 @@ unsigned long previousMillis = 0;
 long int meter;
 bool IsClosed = true; // Cycle starts closed
 int counter = 0;
-int humid;
-int temp;
 String input;
-
-// Calling DHT function w/ defined parameters
-DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   // Serial -> Input/Output  
   Serial.begin(9600);
   Serial.println(">> Starting");
-
-  // DHT -> Temperature-Humidity sensor
-  dht.begin();
 
   // Setting motor pins
   pinMode(pmotor, OUTPUT); pinMode(nmotor, OUTPUT);
@@ -73,6 +60,8 @@ void loop() {
       Serial.print("\tREAD: "); Serial.print(Read); Serial.print("\t\t");
       Serial.print("MTR: "); Serial.print(meter); Serial.print("\t\t");
       Serial.print("CLSD: "); Serial.print(IsClosed); Serial.print("\t\t");
+      Serial.print("MS_OPN: "); Serial.print(digitalRead(ms_fullopen)); Serial.print("\t\t");
+      Serial.print("MS_CLS: "); Serial.print(digitalRead(ms_fullclose)); Serial.print("\t\t");
       Serial.print("\n");
     }
 
@@ -84,73 +73,52 @@ void loop() {
     }
     // Starts opening cycle if meter value reaches 150
     if(meter > 0) { 
-      if(meter > 150 && IsClosed == true) { open(); IsClosed = false; }
+      if(meter > 150 && IsClosed == true) { open(); }
       // If open, subtracts from meter value until it reaches zero
       meter-- ;
     }
-    // if meter value reaches zero, starts closing cycle
-    if(meter == 0 && IsClosed == false) { close(); IsClosed = true; }
+    // if meter value reaches one, starts closing cycle
+    if(meter == 1 && IsClosed == false) { close(); }
 
 
     counter++ ;
-  }
-  if(DHTISUP == true) {
-      // Read temperature and humidity from DHT sensor
-      // and output to serial
-      dhtread();
-  }
+    
   // Receive serial commands  
   if(Serial.available()) {
     input = Serial.readStringUntil('\n');
     if(input == "open") { open(); }
     else if(input == "close") { close(); }
-    else if(input == "dht") { dhtread(); }
     else { Serial.println(">> Unknown command: "+input); }
     }
+  }
 }
 
 void open() {
   // Starts the opening cycle
   Serial.println(">> Opening ...");
+  Serial.println(">> Waiting for MS detection to stop");
 
-  //for(int i = 0; i < 256; i++) { analogWrite(pin, i); delay(2); }
-
-  // Motor power interval (ms)
-  int t = 5000;
-
-  // Set N pin HIGH for `t` seconds
+  // Wait until lid is fully open
+  while(digitalRead(ms_fullopen) == false) {
   digitalWrite(pmotor, LOW); digitalWrite(nmotor, HIGH);
-  delay(t);
+  }
   
   // Send HH to Relay - motor poweroff
+  Serial.println(">> MS detected - Stopped!");
   digitalWrite(pmotor, HIGH); digitalWrite(nmotor, HIGH);
+  IsClosed = false;
 }
 
 void close() {
   // Starts the closing cycle
   Serial.println(">> Closing ...");
+  Serial.println(">> Waiting for MS detection to stop");
 
-  //for(int i = 255; i > 0; i--) { analogWrite(pin, i); delay(2); }
-
-  // Motor power interval (ms)
-  int t = 5000;
-
-  // Set P pin HIGH for `t` seconds
-  digitalWrite(nmotor, LOW); digitalWrite(pmotor, HIGH);
-  delay(t);
-  
-  // Send HH to Relay - motor poweroff
-  digitalWrite(pmotor, HIGH); digitalWrite(nmotor, HIGH);
-}
-
-void dhtread() {
-  int humid = dht.readHumidity();
-  delay(100);
-  int temp = dht.readTemperature();
-  delay(100);
-  if(! isnan(humid) && ! isnan(temp)){
-    Serial.print("TEMP: "); Serial.print(temp); Serial.print(" C\t\t");
-    Serial.print("HUMID: "); Serial.print(humid); Serial.print(" %\t\t");
-    Serial.println();
+  while(digitalRead(ms_fullclose) == false) {
+    digitalWrite(nmotor, LOW); digitalWrite(pmotor, HIGH);
   }
+  // Send HH to Relay - motor poweroff
+  Serial.println(">> MS detected - Stopped!");
+  digitalWrite(pmotor, HIGH); digitalWrite(nmotor, HIGH);
+  IsClosed = true;
 }
